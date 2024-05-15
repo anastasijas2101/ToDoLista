f1139d9610773b6afe6f867e3868fed4e3fe8788
anastasijas2101 <anastasijas101@gmail.com>
2024-05-15 17:49:15 +0200

    Add toast to KreirajTask page

--- src/features/KreirajTask/KreirajTask.jsx
+++ src/features/KreirajTask/KreirajTask.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Button from '../../components/Button/Button'
 import { useTaskStore } from '../../stores/Tasks.store'
 import './KreirajTask.style.css'
+import { Toast } from 'primereact/toast';
 
 const KreirajTask = () => {
     const [text, setText] = useState();
     const { tasks, addNewTask } = useTaskStore();
+    const toast = useRef(null);
 
     const handleInput = (event) => {
         setText(event.target.value);
@@ -18,18 +20,25 @@ const KreirajTask = () => {
                 title: text,
                 isCompleted: false
             }
-
             addNewTask(taskToAdd);
             setText('');
+            showSuccess();
         }
     }
 
+    const showSuccess = () => {
+        toast.current.show({ life: 3000, severity: 'success', detail: 'Uspješno kreiran task!', closable: false })
+    }
+
     return (
-        <div className='createTaskDiv'>
-            <p>Koji je tvoj sledeći task?</p>
-            <input placeholder="Upiši..." value={text} onChange={handleInput} type='text' />
-            <Button buttonText="Sačuvaj" handleClick={handleAddTask} />
-        </div>
+        <>
+            <div className='createTaskDiv'>
+                <p>Koji je tvoj sledeći task?</p>
+                <input placeholder="Upiši..." value={text} onChange={handleInput} type='text' />
+                <Button buttonText="Sačuvaj" handleClick={handleAddTask} />
+            </div>
+            <Toast ref={toast} position='bottom-left' className='toastStyleBox' />
+        </>
     )
 }
 
